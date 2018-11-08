21257a4ab2b195f1c798c1f402886341dde7b89d
ricardogang <38793465+ricardogang@users.noreply.github.com>
2018-11-08 15:22:21 -0600

    Updated, almost ready GUI

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,26 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "highlighter.h"
+#include <QDesktopWidget>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QDesktopWidget dw ;
+    this->resize(dw.width()*0.7,dw.height()*0.7);
+    this->move(dw.width()*0.2,dw.height()*0.1);
+    ui->btnCompile->setDefaultAction(ui->actionCompile);
+    ui->btnRun->setDefaultAction(ui->actionRun);
+
+
+
+//    QRect screenSize = desktop.availableGeometry(this);
+//    this->setFixedSize(QSize(screenSize.width * 0.7f, screenSize.height * 0.7f));
+
+    Highlighter *highlighter = new Highlighter(ui->txtSourceCode->document());
 }
 
 MainWindow::~MainWindow()
