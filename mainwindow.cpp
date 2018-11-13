c06756ee88d1a2cd8bd8e7068ec2febca21e19f3
ricardogang <38793465+ricardogang@users.noreply.github.com>
2018-11-13 02:30:39 -0600

    Run javaProcess, keep Console cursor at end, slots for javaProcess

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,6 +13,12 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    javaProcess= new QProcess() ;
+    connect(javaProcess, &QProcess::started, this, &MainWindow::processStarted);
+    connect(javaProcess, &QProcess::readyRead, this, &MainWindow::processRead);
+
+    connect(javaProcess, &QProcess::errorOccurred, this, &MainWindow::checkError);
+    connect(javaProcess, SIGNAL(finished(int, QProcess::ExitStatus)), this, SLOT(processExited(int,QProcess::ExitStatus)));
 
     QDesktopWidget dw ;
     this->resize(dw.width()*0.7,dw.height()*0.7);
@@ -22,8 +28,8 @@ MainWindow::MainWindow(QWidget *parent) :
 
 
 
-//    QRect screenSize = desktop.availableGeometry(this);
-//    this->setFixedSize(QSize(screenSize.width * 0.7f, screenSize.height * 0.7f));
+    //    QRect screenSize = desktop.availableGeometry(this);
+    //    this->setFixedSize(QSize(screenSize.width * 0.7f, screenSize.height * 0.7f));
 
     Highlighter *highlighter = new Highlighter(ui->txtSourceCode->document());
 }
@@ -36,66 +42,68 @@ MainWindow::~MainWindow()
 void MainWindow::on_actionCompile_triggered()
 {
     //QString filename = QFileDialog::getSaveFileName(this, tr("Guardar"), "","" );
-        QString filename= "Clase.java";
-        if (filename != "") {
-            QFile file(filename);
+    QString filename= "Clase.java";
+    if (filename != "") {
+        QFile file(filename);
 
 
-            if (file.open(QIODevice::ReadWrite | QIODevice::Truncate)) {
-                QTextStream stream(&file);
-                stream << ui->txtSourceCode->toPlainText();
-                file.flush();
-                file.close();
-            }
-            else {
-                QMessageBox::critical(this, tr("Error"), tr("error"));
-                return;
-            }
+        if (file.open(QIODevice::ReadWrite | QIODevice::Truncate)) {
+            QTextStream stream(&file);
+            stream << ui->txtSourceCode->toPlainText();
+            file.flush();
+            file.close();
         }
+        else {
+            QMessageBox::critical(this, tr("Error"), tr("error"));
+            return;
+        }
+    }
 
-        QProcess compilar;
-        QString exec = "javac";
-        QStringList parametros;
-        parametros << filename ;
-        compilar.start(exec, parametros);
-        if (compilar.waitForFinished()){ // sets current thread to sleep and waits for pingProcess end
-            QString resultado(compilar.readAllStandardError());
-            resultado+= compilar.readAllStandardOutput();
-            if (resultado.length()==0) {
-                ui->txtConsole->setPlainText("COMPILATION SUCCESSFULL") ;
-            } else {
-                ui->txtConsole->setPlainText(resultado);
-            }
+    QProcess compilar;
+    compilar.start("javac", QStringList() << filename );
+    if (compilar.waitForFinished()){ // sets current thread to sleep and waits for pingProcess end
+        QString resultado(compilar.readAllStandardError());
+        resultado+= compilar.readAllStandardOutput();
+        if (resultado.length()==0) {
+            ui->txtConsole->setPlainText("COMPILATION SUCCESSFULL") ;
         } else {
-            ui->txtConsole->setPlainText("ERROR: "+compilar.errorString());
+            ui->txtConsole->setPlainText(resultado);
         }
+    } else {
+        ui->txtConsole->setPlainText("ERROR: "+compilar.errorString());
+    }
+}
+
+void MainWindow::processRead() {
+    ui->txtConsole->appendPlainText(javaProcess->readAll());
+}
+
+void MainWindow::processStarted(){
+    javaProcess->write("@");
+}
+
+void MainWindow::checkError(QProcess::ProcessError err){
+    ui->txtConsole->setPlainText("ERROR: "+javaProcess->errorString());
+}
+
+void MainWindow::processExited(int exitCode, QProcess::ExitStatus status){
+    ui->txtConsole->appendPlainText("PROCESS ENDED") ;
 }
 
 void MainWindow::on_actionRun_triggered()
 {
     QString classname= "Clase";
 
-    QProcess correr;
-    QString exec = "c.bat";
-    QStringList parametros2;
-    parametros2 << classname ;
-    parametros2 << "Clase";
+    javaProcess->start("c.bat", QStringList() << "Clase");
 
-    correr.start(exec, parametros2);
-    if (correr.waitForStarted()){
-        QString resultado(correr.readAllStandardError());
-        resultado+= correr.readAllStandardOutput();
-        ui->txtConsole->setPlainText(resultado);
-    } else {
-        ui->txtConsole->setPlainText(correr.errorString());
-    }
     //correr.write("2\n\r");
-//    std::cout << "2" << endl ;
+    //    std::cout << "2" << endl ;
+
 }
 
 void MainWindow::on_btnSend_clicked()
 {
-   /* QProcess correr;
+    /* QProcess correr;
     correr.open();
     QString exec = "cls";
     QStringList parametros;
@@ -112,4 +120,11 @@ void MainWindow::on_btnSend_clicked()
     }*/
 
     std::cout << "hey";
+}
+
+void MainWindow::on_txtConsole_cursorPositionChanged()
+{
+    QTextCursor c(ui->txtConsole->document()) ;
+    c.movePosition(QTextCursor::End);
+    ui->txtConsole->setTextCursor(c);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -1,5 +1,6 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
+#include <QProcess>
 
 #include <QMainWindow>
 
@@ -10,10 +11,14 @@ class MainWindow;
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
+QProcess *javaProcess;
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void processStarted();
+    void processRead() ;
+    void checkError(QProcess::ProcessError err);
+    void processExited(int exitCode, QProcess::ExitStatus status);
 
 private slots:
     void on_actionCompile_triggered();
@@ -21,6 +26,8 @@ private slots:
     void on_actionRun_triggered();
 
     void on_btnSend_clicked();
+
+    void on_txtConsole_cursorPositionChanged();
 
 private:
     Ui::MainWindow *ui;
